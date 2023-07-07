b65fb2b5a2ca6d58371d701be4cf9be18c1ec824
Khan <nwazirwal@gmail.com>
2023-07-07 00:01:05 -0400

    Change 7

--- src/PPPP/PPPP.java
+++ src/PPPP/PPPP.java
@@ -4,11 +4,13 @@ public class PPPP {
 
 	public static void main(String[] args) {
 		
+		System.out.println("May Allah succeed me in my interview");
 		
 		System.out.println("I am practicing");
 		System.out.println("This is my second practice");
 		System.out.println("I am practicing");
-		System.out.println("May Allah succeed me in my interview");
+	
+		System.out.println("I am practicing pull request");
 		System.out.println("I am practicing pull request");
 		
 	}
